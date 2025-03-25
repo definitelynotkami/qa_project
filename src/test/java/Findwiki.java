fe84ec0a32d4df61b019575ee644605b0bea4ad8
Kamilla Sagutdinova <kamilla1a@mail.ru>
2025-03-25 21:01:13 +0300

    updated

--- src/test/java/Findwiki.java
+++ src/test/java/Findwiki.java
@@ -3,17 +3,17 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
-public class WikiFind {
+public class Findwiki {
     @Test
-    void WikiSelenide () {
-    //Откройте страницу Selenide в Github
+    void wikiSelenide () {
+        //Откройте страницу Selenide в Github
         open("https://github.com/selenide/selenide");
-    //Перейдите в раздел Wiki проекта
+        //Перейдите в раздел Wiki проекта
         $("#wiki-tab").click();
-    //Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        //Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
         $("#wiki-pages-filter").setValue("soft");
         $("a[href='/selenide/selenide/wiki/SoftAssertions']").shouldHave(text("Soft assertions")).click();
-    //Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+        //Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
         $(".markdown-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
                 "class Tests {\n" +
                 "  @Test\n" +
